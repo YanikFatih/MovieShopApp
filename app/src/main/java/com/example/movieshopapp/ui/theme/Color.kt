d38140c01ba07c9a9d77e21161aefd70d427cfc0
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2025-01-09 20:19:13 +0300

    colors added

--- app/src/main/java/com/example/movieshopapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/movieshopapp/ui/theme/Color.kt
@@ -9,3 +9,8 @@ val Pink80 = Color(0xFFEFB8C8)
 val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+val MainColor = Color(0xFF1A171A)
+val InfoBoxColor = Color(0xFF272427)
+val TextColor = Color(0xFFC6C3C6)
+val ButtonColor = Color(0xFFF71B23)
