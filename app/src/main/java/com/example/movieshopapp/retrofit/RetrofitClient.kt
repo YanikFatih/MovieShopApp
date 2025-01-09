010265e5345a5a77b95c9762b5c142c4c55ca3f4
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2025-01-09 20:21:34 +0300

    init

--- app/src/main/java/com/example/movieshopapp/retrofit/RetrofitClient.kt
+++ app/src/main/java/com/example/movieshopapp/retrofit/RetrofitClient.kt
@@ -1,10 +1,20 @@
 package com.example.movieshopapp.retrofit
 
+import okhttp3.ResponseBody
+import retrofit2.Converter
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.lang.reflect.Type
 
-class RetrofitClient {
+class RetrofitClient() {
     companion object {
+        /*val nullOnEmptyConverterFactory = object : Converter.Factory() {
+            fun converterFactory() = this
+            override fun responseBodyConverter(type: Type, annotations: Array<out Annotation>, retrofit: Retrofit) = object : Converter<ResponseBody, Any?> {
+                val nextResponseBodyConverter = retrofit.nextResponseBodyConverter<Any?>(converterFactory(), type, annotations)
+                override fun convert(value: ResponseBody) = if (value.contentLength() != 0L) nextResponseBodyConverter.convert(value) else null
+            }
+        }*/
         fun getClient(baseUrl:String) : Retrofit {
             return Retrofit.Builder()
                 .baseUrl(baseUrl)
