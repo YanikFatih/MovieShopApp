4a1464754fb3d27ef565027eb04cf6a7c4493be2
Fatih Yanık <69479457+YanikFatih@users.noreply.github.com>
2025-01-11 04:15:17 +0300

    room implementation

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,4 +80,10 @@ dependencies {
 
     //Glide
     implementation ("com.github.skydoves:landscapist-glide:1.4.4")
+
+    //Room
+    implementation ("androidx.room:room-runtime:2.6.1")
+    kapt("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
 }
